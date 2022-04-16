39d8aec7d9cf65787013fd2b0dcb72e6f17bf031
maddy42031 <madhavanchinnasamy409@gmail.com>
2022-04-16 08:20:15 +0530

    BinarySearch using JS

---
+++ index.js
@@ -0,0 +1,9 @@
+const insertNode = document.getElementById('insertNode');
+const searchNode = document.getElementById('searchNode');
+const clearNode = document.getElementById('clearNode');
+
+class BinarySearch{
+  constructor(){
+    this.node = [];
+  }
+}
